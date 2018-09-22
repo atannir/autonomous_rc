aff3621ce8c2b866a734cead9c625887939f621b
Adam Tannir <atannir@gmail.com>
2018-09-22 15:40:55 -0400

    Added preliminary frame capture
    Written on laptop to be checked out on Pi

--- 4x4fork/state_recorder.py
+++ 4x4fork/state_recorder.py
@@ -10,15 +10,15 @@ import controller_capture as ctl
 
 init_delay = 5 # feels like a long time if you are waiting
 saved_frames = 0
-frame_delay = 0.05
-max_frames = 100
+frame_delay = 0.1 # 20 fps 0.05, 10 fps = 0.1
+max_frames = 500
 start_time = time()
-end_time = None
 
 filename_base = "capture_" + str(time()) + "_out"
 
 last_frame = None
-
+last_frame_time = None
+end_time = None
 
 def init(output_folder = None):
     global filename_base #, cap
@@ -43,12 +43,13 @@ def getOutFile():
     return './' + filename_base + '/' + str(time()) + "-" + filename_base  + "-" + ctl.getPinState()  + ".png"
 
 def saveFrame():
-    global saved_frames, last_frame    
+    global saved_frames, last_frame, last_time
     # removed frame counter check
     # removed display logic here cv2.imshow('frame', frame)
     # removed if cv2.waitKey(1) & 0xFF == ord('q') / break (in try)
     frame = img.returnFrame()
     last_frame = frame
+    last_frame_time = time()
     outfile = getOutFile()
     cv2.imwrite(outfile, frame)
     saved_frames =+ 1
@@ -57,8 +58,16 @@ def getLastFrame():
     return last_frame
 
 if __name__ == '__main__':
-    init("maintest")
-    print(ctl.getPinState())
-    saveFrame()
+    #init("maintest")
+    init()
+    #print(ctl.getPinState())
+    #saveFrame()
+    while ((saved_frames <= max_frames) and (cv2.waitKey(1) & 0xFF != ord('q') )):
+        # will waitKey work if we don't have a display window?
+        cv2.imshow('feed', img.returnFrame())
+        if((last_frame_time + frame_delay) < time()):
+            saveFrame()
+        #pass
+        sleep(0.01) # try not to peg the CPU displaying frames
 
 atexit.register(finish)
