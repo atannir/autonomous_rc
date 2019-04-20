9b27e0859fabe41d64efd1011343740548405a7a
Adam Tannir <atannir@gmail.com>
2019-04-20 19:04:28 -0400

    Start of data curator layout and logic

---
+++ data_curator/curate.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import qtpy # not qt, nor pyqt, qt5, pyqt5...
+# Could have just said 'cutie pie' -> qtpy
+import os
+
+startdir = os.getcwd()
+
+#print(startdir)
+
+# Layout should be graphic on the right and file list on the left.
+# There will be a small bar under the graphic with play and stop
+# Depending on layout, might change modify buttons to be under file list
+# Or have all controls and modify be on the left side.
+
+# Create window
+# Make setup
+# Get file list based on filter
+
+
+
+
+
+
