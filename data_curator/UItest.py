d789fa877b6cf48158e86be460b58267fc5c70be
Adam Tannir <atannir@gmail.com>
2019-04-23 17:10:52 -0400

    Testing UI before committing to design and code

---
+++ data_curator/UItest.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+# Clean up once we know which parts we want
+
+import os
+
+startdir = os.getcwd()
+
+# Frame with horizontal layout
+# Right side: Graphic with bar underneath
+# Ride side under-bar: play - stop
+# Right side under-bar 2: modify buttons FSBLNR-MX (modify, exclude)
+# Left side: file list
+# Left side top: path shooser
+# Left side top 2: filename filter
+# Left side middle: list of files
+# Left side bottom: filename filter if not upder path chooser
+
+
