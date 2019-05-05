473c4cba7476d99305e6af34bff8ce81d5a1523a
Adam Tannir <atannir@gmail.com>
2019-05-05 19:51:37 -0400

    Functional exchange rate example

--- data_curator/guitest/converter.py
+++ data_curator/guitest/converter.py
@@ -5,6 +5,7 @@
 import sys
 #import urllib3
 import requests
+import inspect
 
 #from PyQt5.QtCore import *
 #from PyQt5.QtGui import *
@@ -46,14 +47,21 @@ class Form(QtWidgets.QDialog):
         # self.connect(self.fromSpinBox, SIGNAL("valueChanged(double)"), self.updateUi)
         # self.setWindowTitle("Currency")
         #pass
+        self.fromComboBox.currentIndexChanged.connect(self.updateUi)
+        self.toComboBox.currentIndexChanged.connect(self.updateUi)
+        self.fromSpinBox.valueChanged.connect(self.updateUi)
         # TODO add above as below to connect the components
 
     def updateUi(self):
-            # to = unicode(self.toComboBox.currentText())
-            # from_ = unicode(self.fromComboBox.currentText()) # need _ since from is reserved
-            # amount = (self.rates[from_] / self.rates[to]) * self.fromSpinBox.value()
-            # self.toLabel.setText("%0.2f" % amount)
-        pass
+        # to = unicode(self.toComboBox.currentText())
+        # from_ = unicode(self.fromComboBox.currentText()) # need _ since from is reserved
+        # amount = (self.rates[from_] / self.rates[to]) * self.fromSpinBox.value()
+        # self.toLabel.setText("%0.2f" % amount)
+        #pass
+        to = self.toComboBox.currentText()
+        from_ = self.fromComboBox.currentText()
+        amount = (self.rates[from_] / self.rates[to]) * self.fromSpinBox.value()
+        self.toLabel.setText("%0.2f" % amount)
 
     def getdata(self):
         # Idea from Python Cookbook, as listed in Rapid GUI Programming
@@ -70,13 +78,16 @@ class Form(QtWidgets.QDialog):
             # Also not in the right format.
             #print(fh.content)
 
-            fx_lines = """
-            
-            """
+            fx_lines = inspect.cleandoc("""#
+            Date (<m>/<d>/<year>),01/05/2017
+            Closing Can/US Exchange Rate,1.1725
+            U.S Dollar,1.1755
+            Argentina Peso,0.3797
+            Australian Dollar,0.9164""")
 
             
-            for line in fx_lines:
-                #print(line)
+            for line in fx_lines.splitlines():
+                print(line)
                 line = line.rstrip()
                 if not line or line.startswith(("#", "Closing ")):
                     continue
